dd905c51c92990f4feaddfe5ab6caf8718b9e77e
Arash Khajelou <a.khajelou@gmail.com>
2019-01-07 22:27:44 +0330

    Solve challenge number 11.

---
+++ 11.py
@@ -0,0 +1,20 @@
+# http://www.pythonchallenge.com/pc/return/5808.html
+
+from PIL import Image, ImageDraw
+
+file_address = 'assets/cave.jpg'
+img = Image.open(file_address)
+
+new_img_1 = Image.new('RGB', (img.width, img.height), 'black')
+new_img_1_pixels = new_img_1.load()
+new_img_2 = Image.new('RGB', (img.width, img.height), 'black')
+new_img_2_pixels = new_img_2.load()
+
+for xPos in range(img.width):
+    for yPos in range(img.height):
+        if (yPos + xPos) % 2 != 0:
+            new_img_1_pixels[xPos, yPos] = img.getpixel((xPos, yPos))
+        else:
+            new_img_2_pixels[xPos, yPos] = img.getpixel((xPos, yPos))
+
+new_img_2.show()  # evil is the result
