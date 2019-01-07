32b24e884ba8cb4ed35b0f8925a4605695e2476d
Arash Khajelou <a.khajelou@gmail.com>
2019-01-07 23:03:40 +0330

    Solve challenge number 13.

---
+++ 13.py
@@ -0,0 +1,9 @@
+# http://www.pythonchallenge.com/pc/return/disproportional.html
+
+import xmlrpc.client
+
+conn = xmlrpc.client.ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
+conn_methods = conn.system.listMethods()
+print(conn_methods)
+print(conn.phone('Bert'))
+# the phone number is 555-ITALY
